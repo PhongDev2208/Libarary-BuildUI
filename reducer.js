6e4ff1dcbe68776efa2f430b1daeec898c52917f
Luong Thanh Phong <luongphong22082003@gmail.com>
2023-06-07 20:56:51 +0700

    first commit

---
+++ reducer.js
@@ -0,0 +1,17 @@
+const init = {
+    cars: ['BMW']
+}
+
+export default function reducer(state = init, action, args) {
+    switch (action) {
+        case 'ADD':
+            const [newcar] = args
+            return {
+                ...state,
+                cars: [...state.cars, newcar]
+            }
+            break;
+        default: 
+            return state
+    }
+}
